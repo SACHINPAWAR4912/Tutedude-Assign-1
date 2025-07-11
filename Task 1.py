db187a964c9e7ad0f60005bdcd4e4d33aee04f2e
Sachin Deepak Pawar <143867060+SACHINPAWAR4912@users.noreply.github.com>
2025-07-11 20:14:36 +0530

    Add files via upload

---
+++ Task 1.py
@@ -0,0 +1,13 @@
+#numeric operations
+u=input("First Number: ")
+s=input("Second Number: ")
+u=int(u)
+s=int(s)
+p=u-s
+q=u+s
+r=u*s
+t=u/s
+print("Subtraction: ",p)
+print("Addition: ",q)
+print("Multiplication: ",r)
+print("Division: ",t)
---
+++ Task 2.py
@@ -0,0 +1,5 @@
+##Personalized greeting Message
+p=input("Enter first name: ")
+q=input("Enter last name: ")
+p+=q
+print("Hello, "+p+"! Welcome to Python program.")
